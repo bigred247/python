86bfc3d13ba2aecae5e12d13cc956c35896849ab
bigred247 <47668657+bigred247@users.noreply.github.com>
2020-12-14 10:42:19 +0000

    Delete sec_start_stop_terminate.py

--- sec_start_stop_terminate.py
+++
@@ -1,8 +0,0 @@
-import boto3
-client = boto3.client('ec2')
-
-resp = client.terminate_instances(InstanceIds=['i-0158ab7a03bb6a954'])
-
-for instance in resp['TerminatingInstances']:
-    print("The instance with id {} Terminated".format(instance['InstanceId']))
-
