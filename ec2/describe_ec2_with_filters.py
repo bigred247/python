dbab8c0510838e15f361b85d504f3234e7e750dc
bigred247 <47668657+bigred247@users.noreply.github.com>
2020-12-21 10:47:53 +0000

    Create describe_ec2_with_filters.py

---
+++ ec2/describe_ec2_with_filters.py
@@ -0,0 +1,12 @@
+import boto3
+client = boto3.client('ec2')
+response = client.describe_instances(Filters=[{
+    'Name': 'instance-state-name',
+    'Values': ['running',
+               'pending']
+}])
+
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print("InstanceId is {}".format(instance['InstanceId']))
+        print("InstanceType is {}".format(instance['InstanceType']))
