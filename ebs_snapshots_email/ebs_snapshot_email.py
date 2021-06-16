a1f813078fcae1fe8ac63cba4e2e74825a6d6086
bigred247 <fraz_mahmud@hotmail.com>
2021-06-16 11:45:44 +0100

    changed ARN

--- ebs_snapshots_email/ebs_snapshot_email.py
+++ ebs_snapshots_email/ebs_snapshot_email.py
@@ -18,7 +18,7 @@ for instance in ec2.instances.filter(Filters=backup_filter):
         snapshot_ids.append(snapshot.snapshot_id)
 
 sns_client.publish(
-    TopicArn='arn:aws:sns:eu-west-2:500441578972:snapshots',
+    TopicArn='arn:aws:sns:eu-west-2:500441578972:alerts',
     Subject='EBS Snapshots for web server',
     Message= str(snapshot_ids) 
 )
