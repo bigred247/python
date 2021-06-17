a33835cdff79657ac541c6abdc8903a5bfc150f7
bigred247 <fraz_mahmud@hotmail.com>
2021-06-17 12:37:22 +0100

    edited region settings

--- ami/my-create-image-copy-to-different-region.py
+++ ami/my-create-image-copy-to-different-region.py
@@ -1,14 +1,15 @@
 import boto3
+boto3.setup_default_session(profile_name='eng', region_name='eu-west-2')
 
 ##########################
 ## Part-1 Create Images ##
 ##########################
 
-source_region = 'ap-south-1'
+source_region = 'eu-west-2'
 ec2 = boto3.resource('ec2', region_name=source_region)
 
 
-instances = ec2.instances.filter(InstanceIds=['i-0067eeaab6c8188fd'])
+instances = ec2.instances.filter(InstanceIds=['i-0ee1794ccfc47ea57'])
 
 image_ids = []
 
@@ -42,7 +43,7 @@ waiter.wait(Filters=[{
 destination_region = 'us-east-1'
 client = boto3.client('ec2', region_name=destination_region)
 for image_id in image_ids:
-    client.copy_image(Name='Boto3 Copy'+image_id, SourceImageId=image_id, SourceRegion='ap-south-1')
+    client.copy_image(Name='Boto3 Copy'+image_id, SourceImageId=image_id, SourceRegion='eu-west-2')
 
 
 
