2eb1f6e244d13bb4440250bdbf530c06b0200866
bigred247 <fraz_mahmud@hotmail.com>
2021-06-19 10:57:22 +0100

    pcep training

---
+++ pcep-exercises/print.py
@@ -0,0 +1,5 @@
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+
+print('\n','Your income is', income, 'and you would pay', income * lowtaxland_rate, 'income tax in Lowtaxland or', income * ripoffland_rate, 'income tax in Ripoffland. You would save', income * ripoffland_rate - income * lowtaxland_rate, 'by paying taxes in Lowtaxland!','\n')
